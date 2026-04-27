aabfa8c0219a354ecc31fdabe58dc328b94df460
Rasol07 <poloa0709@gmail.com>
2026-04-27 17:05:19 +0900

    이모지 추가 및 내역 추가 스크롤 작동 확인

--- story.js
+++ story.js
@@ -10,3 +10,18 @@ $('.etc-icon').click(function(){
     }
     console.log(clickCount);
 })
+
+let emojiPlusCount = false;
+$('.emoji-plus').click(function(){
+    if(emojiPlusCount == false) {
+        // $('.emoji-addition').show(); -> 이렇게 하면 display : block 해버려서 깨짐
+        $('.emoji-addition').css('display', 'flex');
+        emojiPlusCount = true;
+    }
+    else {
+        $('.emoji-addition').hide();
+        emojiPlusCount = false;
+    }
+})
+
+// 스크롤 시 hide 하는 기능 구현해야함
