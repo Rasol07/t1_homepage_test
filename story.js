d45bbd08cb0fb0af7bf83c541b4ff1822530448c
Rasol07 <poloa0709@gmail.com>
2026-04-28 17:53:10 +0900

    top-button / 이모지 수정

--- story.js
+++ story.js
@@ -81,3 +81,21 @@ $(document).click(function(e) {
 
 // // 스크롤 시 hide 하는 기능 구현해야함
 
+// top 버튼 구현
+const upButton = $('.up-button');
+upButton.click(function(){
+    window.scrollTo({
+        top : 0,
+        behavior : 'smooth'
+    });
+})
+
+// top 버튼 위치에 따른 hide / show 
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY > 1) {
+        upButton.css('display', 'flex');
+    }
+    else {
+        upButton.css('display', 'none');
+    }
+})
